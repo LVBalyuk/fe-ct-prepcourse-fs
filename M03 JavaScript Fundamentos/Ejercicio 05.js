e3c1995781b287123384078aea4ee03e17af319a
¨valentina¨ <ludmilabaluk10@gmail.com>
2023-07-24 13:27:56 -0400

    Resolucion del ejercicio 05

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -64,7 +64,7 @@ function deEuroAdolar(euro) {
    // Supongamos que 1 euro equivale a 1.20 dólares.
    // Debes calcular el valor recibido como argumento pasándolo a dolares.
    // Tu código:
-   return dolares = 1.20 * euro;
+   return dolares = 1.20 * euro ;
 }
 
 function esVocal(letra) {
